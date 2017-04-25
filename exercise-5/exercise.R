399b6d793822c272808bbe6c02b7e3c73cd18499
Emily Zhai <ezhai24@gmail.com>
2017-04-25 14:57:01 -0700

    api request working

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -5,16 +5,21 @@ library(httr)
 
 # Write a function that allows you to specify a movie, that does the following:
 GetReview <- function(movie) {
+  movie <- "Batman"
   
   # Construct a search query using YOUR api key
   # The base URL is https://api.nytimes.com/svc/movies/v2/reviews/search.json?
   # Your parameters should include a "query" and an "api_key"
   # Note: the HTTR library will take care of spaces in the arguments
   # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
-  
+  base.url <- "https://api.nytimes.com/svc/movies/v2/reviews/search.json?"
+  api.key <- "01bc99cbd2164f5c9377bb2b885241e9"
+  query.params <- list(query=movie, api_key=api.key)
+  response <- GET(base.url, query = query.params)
+  body <- content(response, "text")
   
   # Request data using your search query
-  
+  data <- fromJSON(body)
   
   # What type of variable does this return?
   
